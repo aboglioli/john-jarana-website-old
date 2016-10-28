edd131084b05439ab31f5b68f1ee793ceb0cbe17
Alan Boglioli <kiriost.s@gmail.com>
2016-10-28 01:36:13 -0300

    Adding design

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,13 +1,22 @@
 jQuery(function($) {
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  autoplay: true,
-  items: 1,
-  margin: 0,
-  nav: true,
-  navText: ['', ''],
-  autoplayHoverPause: true,
-  animateOut: 'slideOutUp',
-  animateIn: 'slideInUp'
-});
+  // $(".owl-carousel").owlCarousel({
+  //   loop: true,
+  //   autoplay: true,
+  //   items: 1,
+  //   margin: 0,
+  //   nav: true,
+  //   navText: ['', ''],
+  //   autoplayHoverPause: true,
+  //   animateOut: 'slideOutUp',
+  //   animateIn: 'slideInUp'
+  // });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    items: 2,
+    margin: 30,
+    nav: false,
+    autoplayHoverPause: true,
+  });
 });
