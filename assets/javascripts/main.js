3f05d5d2879af9c6ec07711cc9b035c2595c1fc5
Alan Boglioli <kiriost.s@gmail.com>
2016-11-03 04:20:22 -0300

    Contact form

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -21,4 +21,43 @@ jQuery(function($) {
     animateOut: 'slideOutUp',
     animateIn: 'slideInUp'
   });
+
+  /**
+  * Contact form
+  */
+  $('#contact-form').submit(function(e) {
+    e.preventDefault();
+
+    var formObj = $(this);
+    var contactData = formObj.serializeArray();
+
+    // var config = {
+    //   apiKey: "AIzaSyAERhm4hw0QCBBXS_-IL2pcxvv-ZUqrdMk",
+    //   authDomain: "contact-form-91ea2.firebaseapp.com",
+    //   databaseURL: "https://contact-form-91ea2.firebaseio.com",
+    //   storageBucket: "",
+    //   messagingSenderId: "862668692236"
+    // };
+    // firebase.initializeApp(config);
+
+    // Store emails to firebase
+
+    console.log(contactData);
+
+    firebase.auth().signInAnonymously().catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+
+        firebaseContact.database().ref('/contact').push(contactData);
+      }
+    });
+
+  });
 });
